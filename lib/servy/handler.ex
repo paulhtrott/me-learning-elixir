6d5ea7bb4fd01deac1438443f8eb03217cd8300c
Paul Trott <paultrott78@gmail.com>
2017-06-24 19:27:57 -0500

    Lesson 8 changes

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,11 +1,39 @@
 defmodule Servy.Handler do
+  require Logger
+
   def handle(request) do
     request
     |> parse
+    |> rewrite_path
     |> log
     |> route
+    |> track
+    |> emojify
     |> format_response
   end
+
+  def track(%{status: 404, path: path} = conv) do
+    IO.puts "Warning: #{path} is on the loose!"
+    conv
+  end
+
+  def track(conv), do: conv
+
+  def rewrite_path(%{path: "/wildlife"} = conv) do
+    %{ conv | path: "/wildthings" }
+  end
+
+  def rewrite_path(%{path: path} = conv) do
+    regex = ~r{\/(?<thing>\w+)\?id=(?<id>\d+)}
+    captures = Regex.named_captures(regex, path)
+    rewrite_path_captures(conv, captures)
+  end
+
+  def rewrite_path_captures(conv, %{"thing" => thing, "id" => id}) do
+    %{ conv | path: "/#{thing}/#{id}" }
+  end
+
+  def rewrite_path_captures(conv, nil), do: conv
 
   # one liner with a single line return
   def log(conv),  do: IO.inspect conv
@@ -29,29 +57,39 @@ defmodule Servy.Handler do
     }
   end
 
-  def route(conv) do
-    route(conv, conv.method, conv.path)
-  end
+  #def route(conv) do
+  #  route(conv, conv.method, conv.path)
+  #end
 
-  def route(conv, "GET", "/wildthings") do
+  def route(%{ method: "GET", path: "/wildthings" } = conv) do
     %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
   end
 
-  def route(conv, "GET", "/bears") do
+  def route(%{ method: "GET", path: "/bears" } = conv) do
     %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
   end
 
-  def route(conv, "GET", "/bears/" <> id) do
+  def route(%{ method: "GET", path: "/bears/" <> id } = conv) do
     %{ conv | status: 200, resp_body: "Bear #{id}" }
   end
 
-  def route(conv, "DELETE", "/bears/" <> _id) do
+  def route(%{ method: "DELETE", path: "/bears/" <> _id} = conv) do
     %{ conv | status: 403, resp_body: "Deleting a Bear is forbidden!" }
   end
 
-  def route(conv, _method, path) do
+  def route(%{ path: path } = conv) do
+    Logger.error "Danger! You Found Nothing!!"
     %{ conv | status: 404,  resp_body: "No #{path} here!"}
   end
+
+  def emojify(%{ status: 200 } = conv) do
+    Logger.info "you found a 200 baby! congrats!"
+    emojies = String.duplicate("🎉", 5)
+    body = emojies <> "\n" <> conv.resp_body <> "\n" <> emojies
+    %{ conv | resp_body: body }
+  end
+
+  def emojify(conv), do: conv
 
   def format_response(conv) do
     # Use values in the map the create an HTTP response string
@@ -129,4 +167,15 @@ response = Servy.Handler.handle(request)
 
 IO.puts response
 
+request = """
+GET /bears?id=1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
 
